4789abbab4c3280a4863b29395ec9429ceedbb66
BSmooo <williambsmithii@gmail.com>
2024-12-04 10:06:07 -0800

    Update index.js with proper component import

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import MLvsStats from '../components/MLvsStats';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <main className="min-h-screen bg-white">
       <MLvsStats />
-    </div>
+    </main>
   );
 }
