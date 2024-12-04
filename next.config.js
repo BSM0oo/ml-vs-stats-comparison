24705e47b32fac7b96cbbb1c7e5ca217bef0fe00
BSmooo <williambsmithii@gmail.com>
2024-12-04 10:03:49 -0800

    Update Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/ml-vs-stats-comparison' : '',
   images: {
     unoptimized: true
-  }
+  },
 };
 
 module.exports = nextConfig;
