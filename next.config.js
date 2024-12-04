fd6eb5359ab14740cdf05093950346eee1196d9c
BSmooo <williambsmithii@gmail.com>
2024-12-04 10:02:24 -0800

    Remove old workflow and update Next.js config

--- next.config.js
+++ next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/ml-vs-stats-comparison',
   images: {
     unoptimized: true
-  },
-  assetPrefix: '/ml-vs-stats-comparison/',
-  trailingSlash: true
+  }
 };
 
 module.exports = nextConfig;
